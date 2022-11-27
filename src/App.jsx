9c07012b728e9968a3020c1c31e826f955f014f5
medhavii <medhavimehta118@gmail.com>
2022-11-27 17:43:14 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import { API_URL } from "./constant.js";
+import MovieCard from "./MovieCard";
+
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setsearchTerm ] = useState('');
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm} 
+          onChange={(e) => setsearchTerm(e.target.value)}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
+        {movies?.length > 0 ? (
+          <div className="conatiner">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No Movies Found</h2>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
