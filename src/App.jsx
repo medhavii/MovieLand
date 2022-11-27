376e980e81511c759a1567ca9968729cbc0827b9
medhavii <medhavimehta118@gmail.com>
2022-11-27 18:20:06 +0530

    container fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import SearchIcon from "./search.svg";
 import { API_URL } from "./constant.js";
 import MovieCard from "./MovieCard";
 
 
+
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const [searchTerm, setsearchTerm ] = useState('');
+  const [searchTerm, setsearchTerm] = useState("");
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
@@ -17,28 +19,35 @@ const App = () => {
   useEffect(() => {
     searchMovies("Batman");
   }, []);
+
   return (
     <div className="app">
       <h1>MovieLand</h1>
+
       <div className="search">
         <input
           placeholder="Search for movies"
-          value={searchTerm} 
+          value={searchTerm}
           onChange={(e) => setsearchTerm(e.target.value)}
         />
-        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
-        {movies?.length > 0 ? (
-          <div className="conatiner">
-            {movies.map((movie) => (
-              <MovieCard movie={movie} />
-            ))}
-          </div>
-        ) : (
-          <div className="empty">
-            <h2>No Movies Found</h2>
-          </div>
-        )}
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
+      
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies Found</h2>
+        </div>
+      )}
     </div>
   );
 };
